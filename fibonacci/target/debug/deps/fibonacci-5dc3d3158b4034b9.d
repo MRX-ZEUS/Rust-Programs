ab9e2add848632c062eaf5276743a41695bbf1fb
MRX <mrx@localhost.localdomain>
2024-10-03 21:39:02 +0530

    added fibonacci series, file IO

---
+++ fibonacci/target/debug/deps/fibonacci-5dc3d3158b4034b9.d
@@ -0,0 +1,5 @@
+/home/mrx/Documents/RUST/fibonacci/target/debug/deps/libfibonacci-5dc3d3158b4034b9.rmeta: src/main.rs
+
+/home/mrx/Documents/RUST/fibonacci/target/debug/deps/fibonacci-5dc3d3158b4034b9.d: src/main.rs
+
+src/main.rs:
---
+++ fibonacci/target/debug/deps/fibonacci-e7ab9204dde4759f.d
@@ -0,0 +1,5 @@
+/home/mrx/Documents/RUST/fibonacci/target/debug/deps/libfibonacci-e7ab9204dde4759f.rmeta: src/main.rs
+
+/home/mrx/Documents/RUST/fibonacci/target/debug/deps/fibonacci-e7ab9204dde4759f.d: src/main.rs
+
+src/main.rs:
